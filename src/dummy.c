c1c1fcd886bd60b01dd350147c928c16eca402ba
midhan <nitish.midha@hpe.com>
2016-02-24 12:50:05 -0800

    Added CMake file and a dummy executable file to avoid linkage issues.
    Change-Id: Ib3543a23e7dffae4f6270d8b419a8ab9dcfff08b
    Signed-off-by: midhan <nitish.midha@hpe.com>

---
+++ src/dummy.c
@@ -0,0 +1,12 @@
+/* NOTE: This file is just to avoid the linkage issues for CMake file,
+ *       Remove it after adding the HW-Vtep code and change the CMake
+ *       file accordingly.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("OPS-HW-VTEP!\n");
+    return 0;
+}
